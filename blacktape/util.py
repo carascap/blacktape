0e48b9860bac7836b5ea19cae99ef4941274abac
Antoine de Torcy <adetorcy@users.noreply.github.com>
2022-01-19 11:14:28 -0500

    Cleanup

--- blacktape/util.py
+++ blacktape/util.py
@@ -1,11 +1,4 @@
-import logging
-import multiprocessing
 import signal
-from concurrent.futures import ProcessPoolExecutor, as_completed
-from pathlib import Path
-from typing import Callable, List
-
-logger = multiprocessing.log_to_stderr(level=logging.INFO)
 
 
 def worker_init():
@@ -16,21 +9,3 @@ def worker_init():
     """
 
     signal.signal(signal.SIGINT, signal.SIG_IGN)
-
-
-def process_files(files: List[Path], job: Callable, *args, **kwargs) -> None:
-    futures = []
-
-    with ProcessPoolExecutor(initializer=worker_init) as executor:
-
-        # Iterate over files
-        for f in files:
-            futures.append(executor.submit(job, f, *args, **kwargs))
-
-        # Handle exceptions in worker processes
-        for future in as_completed(futures):
-            try:
-                res = future.result()
-                # Make ORM object from res
-            except Exception as exc:
-                logger.exception(exc)
